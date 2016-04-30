1f741abcbb1cf33451c649bb7a1283a14a34113e
ChouUn <chouunsoft@gmail.com>
2016-04-30 16:24:27 +0800

    update at 201604301624

--- hexo_code_copy.bat
+++
@@ -1,5 +0,0 @@
-::IF EXIST "D:\Hexo\source\code\Template" (
-::    rmdir "D:\Hexo\source\code\Template" /s /q
-::)
-xcopy /E /Y /I "D:\Users\Documents\WorkSpaceCpp\CodeWorld\Template" "D:\Hexo\source\code\Template"
-xcopy /E /Y /I "D:\Users\Documents\WorkSpaceCpp\CodeWorld\Library" "D:\Hexo\source\code\Library"
